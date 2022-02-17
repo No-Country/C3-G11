0952fca653d7063a692d64dd4d5061f6641ce6ca
Celso Zabala <celsobrc@gmail.com>
2022-02-17 19:13:07 -0300

    Basic user and role MVC with security configuration

--- backend/src/main/java/ecommerce/g11/noCountry/servicios/UserService.java
+++ backend/src/main/java/ecommerce/g11/noCountry/servicios/UserService.java
@@ -5,13 +5,33 @@
  */
 package ecommerce.g11.noCountry.servicios;
 
+import ecommerce.g11.noCountry.entidades.User;
+import ecommerce.g11.noCountry.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
- *
  * @author usuario
  */
 @Service
 public class UserService {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        userRepository.save(user);
+        return user;
+
+    }
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
 }
