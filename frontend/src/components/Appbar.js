8a1f3082353d7d70cb9ad688e14a874472ebf55c
CanCerber0 <geronimo.bknuttzen@gmail.com>
2022-02-21 09:18:19 -0300

    para Pull

--- frontend/src/components/Appbar.js
+++ frontend/src/components/Appbar.js
@@ -11,6 +11,7 @@ const useStyles = makeStyles( (theme) => ({
     root: {
         flexGrow: 1,
         textAlign: "center",
+        backgroundColor:"red",
     },
     menuButton: {
         marginRight: theme.spacing(2)
