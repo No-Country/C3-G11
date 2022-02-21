a54a9cd12358fd424f4a007c87b076151819c172
Geronimo Daniel Banegas Knuttzen <88806192+CanCerber0@users.noreply.github.com>
2022-02-21 09:10:30 -0300

    Revert "Geronimo dev"

--- backend/src/main/java/ecommerce/g11/noCountry/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ecommerce.g11.noCountry.repository;
-
-import ecommerce.g11.noCountry.entidades.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
