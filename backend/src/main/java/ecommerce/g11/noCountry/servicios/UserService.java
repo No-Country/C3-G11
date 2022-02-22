f0f6aeef7040f017ac12b3b125c04bc6149e81fd
facuh5 <facu5herraez@gmail.com>
2022-02-22 01:29:14 -0300

    method for edit Users

--- backend/src/main/java/ecommerce/g11/noCountry/servicios/UserService.java
+++ backend/src/main/java/ecommerce/g11/noCountry/servicios/UserService.java
@@ -9,6 +9,7 @@ import ecommerce.g11.noCountry.entidades.User;
 import ecommerce.g11.noCountry.enums.Roles;
 import ecommerce.g11.noCountry.errores.ErrorService;
 import ecommerce.g11.noCountry.repositorios.UserRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,9 @@ public class UserService {
     @Autowired
     private UserRepository uR;
     
+    
+    // create: String, String, String, Roles, String, String, int, String, int ---> User
+    // this method will be used for create a new User.
     public User create (String name, String lastN, String mail, Roles rol, String pass, String pass2, int phone, String adress, int dni)throws ErrorService{
         checkDatesCreate(name, lastN, mail, rol, pass, pass2, phone, adress, dni);
         User user = new User();
@@ -33,6 +37,14 @@ public class UserService {
 //        user.SetAdress(adress);
 //        user.SetDni(dni);
         return uR.save(user);
+    }
+    
+    // edit: String, String, String, String, int, String, int ---> none
+    // this method will be used for edit an exist User and save the changes.
+    public void edit(String idUser, String name, String lastN, String mail, int phone, String adress, int dni)throws ErrorService{
+        User user = findById(idUser);
+        user = checkEditDates(user, name, lastN, mail, phone, adress, dni);
+        uR.save(user);
     }
     
     ////////////////////////////////////////////////////////////////////////////
@@ -74,9 +86,44 @@ public class UserService {
         }
     }
     
+    private User checkEditDates (User user, String name, String lastN, String mail, int phone, String adress, int dni)throws ErrorService{
+        if(name != null && !name.isEmpty()){
+//            user.setName(name);
+        }
+        if(lastN != null && !lastN.isEmpty()){
+//            user.setLastName(lastN);
+        }
+        if(mail != null && !mail.isEmpty()){
+//            user.setMail(mail);
+        }
+        if(adress != null && !adress.isEmpty()){
+//            user.setAdress(adress);
+        }
+        String num = String.valueOf(phone);
+        if(num != null && !num.isEmpty()){
+//            user.setPhoneNumber(phone);
+        }
+        String doc = String.valueOf(dni);
+        if(doc != null && !doc.isEmpty()){
+//            user.setDni(dni);
+        }
+        return user;
+    }
+    
     ////////////////////////////// QUERYS //////////////////////////////////////
     
+    // countByMail: count the number of Users whit the same mail as the one entered.
     private int countByMail(String mail)throws ErrorService{
         return uR.countByMail(mail);
+    }
+    
+    // findById: find the User with the id entered.
+    private User findById(String id)throws ErrorService{
+        Optional<User> us = uR.findById(id);
+        if(us.isPresent()){
+            return us.get();
+        }else{
+            throw new ErrorService("No se ha encontrado el usuario al que intenta ingresar");
+        }
     }
 }
