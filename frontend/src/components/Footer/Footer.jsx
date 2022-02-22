e549476bf1fc887439c39afde7336561c05d74cf
Oriana <orilupellegrini@gmail.com>
2022-02-22 12:56:07 -0300

    Modify Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 import styles from './Footer.module.css';
+import ScreenLogo from '../../assets/img/ScreenLogo.jpeg';
 
 export default function Footer() {
     return (
           <footer className={styles.siteFooter}>
-            <div className={styles.container.sections}>
+            
               <a href="#">¿Cómo comprar?</a>
               <a href="#">Productos</a>
               <a href="#">Nosotros</a>
-              </div>
-            <div className={styles.container.logo}>
-              <img src="../img/ScreenLogo.jpeg" alt="" />
-            </div>
-            <div className={styles.container.icons}>
-              <i className={styles.container.icon} class="fa-brands fa-instagram"></i>
-              <i className={styles.container.icon} class="fa-brands fa-facebook"></i>
-              <i className={styles.container.icon} class="fa-brands fa-linkedin"></i>
-            </div>
+              
+           
+              <img src={ScreenLogo} alt="Santero" />
+            
+            
+              <i className="fa-brands fa-instagram icon"></i>
+              <i className="fa-brands fa-facebook icon"></i>
+              <i className="fa-brands fa-linkedin icon"></i>
+            
           </footer>
     );
 }
